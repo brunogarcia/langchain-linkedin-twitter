2d7db660f5b7ebbe121a32a2daa13ba5fa431262
Bruno Garcia <bruno.garcia.echegaray@gmail.com>
2023-05-28 16:44:01 +0200

    feat(twitter) use agent and get username

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,5 @@
-from agents import linkedin_agent
-from third_parties import linkedin
+from third_parties import linkedin, twitter
+from agents import linkedin_agent, twitter_agent
 
 
 def ice_break(name: str):
@@ -8,8 +8,13 @@ def ice_break(name: str):
         linkedin_profile_url=linkedin_profile_url
     )
 
+    twitter_username = twitter_agent.lookup(name=name)
+    twitter_data = twitter.scrape_user_tweets(username=twitter_username, num_tweets=5)
+
     print(f"Here is the LinkedIn data: {linkedin_data}")
-    return linkedin_data
+    print(f"Here is the Twitter data: {twitter_data}")
+
+    return linkedin_data, twitter_data
 
 
 if __name__ == "__main__":
